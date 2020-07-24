9a6a53b793b995be22b5d0c2d7f8b7068307e3e2
royeradames <royeraadames@gmail.com>
2020-07-24 11:48:44 -0400

    added a header with 2 button that takes you from home and form

--- src/App.js
+++ src/App.js
@@ -5,12 +5,25 @@ import { Switch, Route, useHistory} from 'react-router-dom'
 //import components
 import Confirmation from './components/Confirmation'
 import Form from './components/Form'
+import Home from './components/Home'
+import Header from './components/Header'
+
 
 const App = () => {
+  const history = useHistory()
+  
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <Header/>
+
+      <Switch>
+        <Route>
+          <Form />
+        </Route>
+        <Route>
+          <Form />
+        </Route>
+      </Switch>
     </>
   );
 };
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {useHistory} from 'react-router-dom'
+
+export default function Header( props){
+    debugger
+    const history = useHistory()
+    return(
+        <div className={'header-container'}>
+            <h1>Lambda Eats</h1>
+            <button onClick={() => history.push('/')}>Home</button>
+            <button onClick={() => history.push('/Form')}>Form</button>
+        </div>
+    )
+}
