44f07c5189202127b6135feef572e19400c8dd24
royeradames <royeraadames@gmail.com>
2020-07-24 15:42:47 -0400

    adding cypress to the form

--- cypress/integration/form.specs.js
+++ cypress/integration/form.specs.js
@@ -1,4 +1,16 @@
 /// <reference types="Cypress" />
 describe('Input and submit form', () => {
-    
+    it('can navigate to the site', ()=> {
+        cy.visit('http://localhost:3000')
+        cy.url().should('include', 'localhost')
+    })
+    it('submit button is disabled', () => {
+        cy.get('.btn-disabled').should('be.disabled')
+    })
+    it('Populate all fields', () => {
+        cy.get('input[name="first_name"]').type('Royer')
+        cy.get('input[name="last_name"]').type('Adames')
+        cy.get('input[name="size"]').click()
+    })
+   
 })
